02283ec46a3cb2c39152b4c38fb29c70fe4252c3
torihazi <lovebarusa19981225@icloud.com>
2024-11-05 18:43:03 +0900

    modify

--- pr3/main.go
+++ pr3/main.go
@@ -5,18 +5,18 @@ import "fmt"
 type MyIntSlice []int
 
 func main() {
-	m := MyIntSlice{1, 2, 2, 3, 3, 3, 4, 5}
+	m := MyIntSlice{4, 2, 2, 3, 3, 3, 4, 5, 1, 2, 4, 5}
 	fmt.Println(m)
 	fmt.Println(m.Unique())
 
 }
 
 func (m MyIntSlice) Unique() []int {
-	var prevValue int
+	var prevValueMap map[int]bool = make(map[int]bool)
 	var result []int
-	for index, currentValue := range m {
-		if index == 0 || prevValue != currentValue {
-			prevValue = currentValue
+	for _, currentValue := range m {
+		if _, ok := prevValueMap[currentValue]; !ok {
+			prevValueMap[currentValue] = !ok
 			result = append(result, currentValue)
 		}
 	}
